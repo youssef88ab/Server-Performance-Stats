4f625ba81b8a0754f92bd937bc375cd041e3e991
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-08-13 17:43:04 +0100

    Add Memory Usage

--- server-stats.sh
+++ server-stats.sh
@@ -2,6 +2,10 @@
 
 echo "======= SERVER STATES =======" 
 
-#Cpu Usage 
+# Cpu Usage 
 cpu=$(top -bn1 | grep "Cpu(s)" | awk '{ print $2 + $4 }')
 echo "CPU Usage: $cpu%"
+
+# Memory Usage 
+free_mem=$(free -m | awk 'NR==2{ printf "%sMb (%.2f%%) \n", $3 , $3*100/$2 }')
+echo "Memory Used: $free_mem"
