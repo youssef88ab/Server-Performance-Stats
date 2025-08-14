984f9b3940aa43e49ac2420b0727319aa72b98cd
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-08-14 15:23:47 +0100

    Add Top 5 Proccesse by CPU Usage

--- server-stats.sh
+++ server-stats.sh
@@ -9,3 +9,12 @@ echo "CPU Usage: $cpu%"
 # Memory Usage 
 free_mem=$(free -m | awk 'NR==2{ printf "%sMb (%.2f%%) \n", $3 , $3*100/$2 }')
 echo "Memory Used: $free_mem"
+
+# Disk Usage 
+disk=$(df -h | grep '^/dev/' | awk '{printf "%-20s %-8s used (Total: %s)\n", $1, $5, $2}')
+echo -e "Disk Usage:\n$disk"
+
+# Top 5 processes by CPU usage
+processes_usage=$(ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 6) 
+echo -e "Top 5 Process Usage:\n$processes_usage"
+
